93930391ec733fcb330db473e9ffc7f15e74e77a
Hara <harachung90@gmail.com>
2022-11-22 20:25:05 +1100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <ul>
+                    <li>ReactFacts</li>
+                    <li>React Course - Project 1</li>
+                </ul>
+            </header>
+            <body className="App-body">
+                <h1>Fun facts about React</h1>
+                <ul>
+                    <li>Was first released in 2013</li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Has well over 100k stars on GitHub</li>
+                    <li>Is maintained by Facebook</li>
+                    <li>Powers thousands of enterprise apps, including mobile apps</li>
+                </ul>
+            </body>
+        </div>
+    );
+}
+
+export default App;
